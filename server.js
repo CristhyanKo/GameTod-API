35d18626f9e3f49419a695395cca499cb65c3888
Cristhyan Kohlhase <cristhyan.ko@outlook.com>
2019-02-15 14:32:18 -0300

    Init

---
+++ server.js
@@ -0,0 +1,23 @@
+const express = require('express')
+const mongoose = require('mongoose')
+const port = (process.env.PORT || 3001)
+const version = (process.env.API_VERSION || 'v1')
+
+mongoose.connect('mongodb://localhost:27017/gametoddb', { useNewUrlParser: true })
+
+const app = express()
+app.use(express.json())
+
+app.use(`/api/${version}`, require('./src/routes/index.route'))
+
+app.listen(port, () => {
+    console.log(
+`
+------------------------------------
+Server listening on port:\x1b[33m ${port}
+
+\x1b[0mURL:\x1b[32m http://localhost:${port} \x1b[0m
+------------------------------------
+`
+    )
+})
---
+++ src/controllers/index.controller.js
@@ -0,0 +1,3 @@
+exports.get = async (req, res) => {
+    res.send('GameTod - API (Version 1.0.0)')
+}
